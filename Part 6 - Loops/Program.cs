c6f9d881833d0b33de913ddc4f7681e1012f123f
Judah Godden <339633265@GoTVDSB.ca>
2024-09-17 09:26:12 -0400

    Add project files.

---
+++ Part 6 - Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6___Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+
+            while (done == false)
+            {
+
+            }
+        }
+    }
+}
